c03fc659ad43d17b2453095a5ec3fcc4dafe1707
Bart <b.vdklundert@outlook.com>
2018-10-09 13:37:38 +0200

    Start of login design

--- client/src/App.js
+++ client/src/App.js
@@ -10,20 +10,6 @@ import store from "./store";
 
 import PrivateRoute from "./components/common/PrivateRouter";
 
-import Navbar from "./components/layout/Navbar";
-import Footer from "./components/layout/Footer";
-import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import Dashboard from "./components/dashboard/Dashboard";
-import CreateProfile from "./components/create-profile/CreateProfile";
-import EditProfile from "./components/edit-profile/EditProfile";
-import AddExperience from "./components/add-credentials/AddExperience";
-import AddEducation from "./components/add-credentials/AddEducation";
-import Profiles from "./components/profiles/Profiles";
-import Profile from "./components/profile/Profile";
-import NotFound from "./components/not-found/NotFound";
-
 import "./App.css";
 
 // Check for token
@@ -53,46 +39,41 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <div className="App">
-            <div className="myContainer">
-              <Navbar />
-              <Route exact path="/" component={Landing} />
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/profiles" component={Profiles} />
-              <Route exact path="/profile/:handle" component={Profile} />
-              <Switch>
-                <PrivateRoute exact path="/dashboard" component={Dashboard} />
-              </Switch>
-              <Switch>
-                <PrivateRoute
-                  exact
-                  path="/create-profile"
-                  component={CreateProfile}
-                />
-              </Switch>
-              <Switch>
-                <PrivateRoute
-                  exact
-                  path="/edit-profile"
-                  component={EditProfile}
-                />
-              </Switch>
-              <Switch>
-                <PrivateRoute
-                  exact
-                  path="/add-experience"
-                  component={AddExperience}
-                />
-              </Switch>
-              <Switch>
-                <PrivateRoute
-                  exact
-                  path="/add-education"
-                  component={AddEducation}
-                />
-              </Switch>
-              <Route exact path="/not-found" component={NotFound} />
-              <Footer />
+            <div className="loginImage">
+              <img
+                className="backgroundImage"
+                src="img/showcase.jpg"
+                alt="backgroundImage"
+              />
+              <div className="imageText">
+                <h1>Laméco</h1>
+                <h4>Maakt Online Succesvol</h4>
+              </div>
+            </div>
+            <div className="loginContainer">
+              <div className="formContainer">
+                <form>
+                  <input
+                    type="text"
+                    name="uname"
+                    placeholder="Email or Username"
+                  />
+                  <input
+                    type="password"
+                    name="psw"
+                    placeholder="Password"
+                    required
+                  />
+                  <label>
+                    <input type="checkbox" name="remember" required /> Remember
+                    me
+                  </label>
+                  <span>
+                    <a href="#">Forgot Password?</a>
+                  </span>
+                  <button type="submit">Login</button>
+                </form>
+              </div>
             </div>
           </div>
         </Router>
