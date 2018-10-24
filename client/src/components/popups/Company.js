034a1a02ea500d04c2bdf8f234120654ae05a8f0
Dylano <dylano.hartman@gmail.com>
2018-10-24 14:42:25 +0200

    Some styling and created popupimage component

--- client/src/components/popups/Company.js
+++ client/src/components/popups/Company.js
@@ -5,9 +5,10 @@ class Company extends Component {
     return (
       <div className="card">
         <div className="cardContent">
-          <div className="cardHeader">
+          <div className="cardHeader ">
             <h1>yoyo</h1>
           </div>
+          <div className="overlayImage" />
           <div className="cardContent">
             <button /> <button />
           </div>
