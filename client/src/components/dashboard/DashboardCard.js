3f2253029420033416aff78121b2dfa2502f8f7d
Mark Hendriks <mark.hendriks9999@gmail.com>
2018-10-31 15:38:12 +0100

    fugly ass snackbar but works

--- client/src/components/dashboard/DashboardCard.js
+++ client/src/components/dashboard/DashboardCard.js
@@ -1,23 +1,57 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import SnackBar from "react-material-snackbar";
 
 class DashboardCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      copied: false
+      copied: false,
+      snackbar: false
     };
   }
+  toggleSnackbar(snackbar) {
+    if (this.state.snackbar) {
+      this.setState(state => ({
+        snackbar: false
+      }));
+    } else {
+      this.setState(state => ({
+        snackbar: true
+      }));
+    }
+  }
   render() {
+    let snackbar = false;
     const { handle } = this.props;
     const { companyhandle } = this.props;
     const linkEdit = `/dashboard-edit/${handle}`;
     const linkShow = `/${companyhandle}/${handle}`;
     const linkShare = `${window.location.href}${companyhandle}/${handle}`;
 
+    if (this.state.snackbar) {
+      snackbar = (
+        <SnackBar
+          show={true} //Boolean  - Required and Default - `false`
+          timer={1000} //Number   - Optional and Default - `4000` (4 secs)
+        >
+          <p>Copied to clipboard</p>
+        </SnackBar>
+      );
+      setTimeout(() => {
+        this.setState(state => ({
+          snackbar: false
+        }));
+      }, 3000);
+    } else {
+      snackbar = null;
+    }
+
     return (
       <div>
+        {snackbar}
+        {console.log(snackbar)}
         <span>{this.props.name}</span>
         <div className="editButtons">
           <Link to={linkEdit}>
@@ -30,7 +64,12 @@ class DashboardCard extends Component {
             text={linkShare}
             onCopy={() => this.setState({ copied: true })}
           >
-            <i className="material-icons">share</i>
+            <i
+              onClick={() => this.setState({ snackbar: true })}
+              className="material-icons"
+            >
+              share
+            </i>
           </CopyToClipboard>
         </div>
       </div>
