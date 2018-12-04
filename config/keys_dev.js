ceeb8f284aa85daa372c68c94f1e3b62f444fb08
MaxAltena <Max@MaxAltena.com>
2018-12-04 22:16:40 +0100

    fixed some potential security vulnerabilities within dependencies & :email: things

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,8 +1,13 @@
 // Keys used when running locally
 module.exports = {
-  mongoURI: "mongodb://admin:admin123@ds113923.mlab.com:13923/lamecodashboard",
-  secretOrKey: "secretOrKey27985415236971579841271975997839145284",
-  mailHost: "smtp02.hostnet.nl",
-  mailAccountUsername: "lamecodashboard@maxaltena.com",
-  mailAccountPassword: "y5546QIXY"
+  mongoURI:
+    "mongodb://admin:admin123@ds113923.mlab.com:13923/lamecodashboard" /* Database link */,
+  secretOrKey:
+    "secretOrKey27985415236971579841271975997839145284" /* Secret key */,
+  mailHost:
+    "smtp02.hostnet.nl" /* This is my email host, change it to yours respectively */,
+  mailAccountUsername:
+    "lamecodashboard@maxaltena.com" /* This is my email username, change it to yours respectively, this is no longer accessible */,
+  mailAccountPassword:
+    "y5546QIXY" /* This is my email password, change it to yours respectively, this is no longer accessible */
 };
